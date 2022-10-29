924e6918867defcee9b7243d8cedc7ba398e296c
cps97 <colinstoutt@gmail.com>
2022-10-29 14:05:39 -0700

    complete meen auth

--- controllers/users.js
+++ controllers/users.js
@@ -4,13 +4,29 @@ const userRouter = express.Router();
 const User = require("../models/user.js");
 const bcrypt = require("bcrypt");
 
+module.exports = userRouter;
+
+// New (registration page)
+userRouter.get("/new", (req, res) => {
+  res.render("users/new.ejs", {
+    currentUser: req.session.currentUser,
+  });
+});
+
+// -=-create-=-
 userRouter.post("/", (req, res) => {
+  console.log("Here is the form data", req.body);
   //overwrite the user password with the hashed password, then pass that in to our database
   req.body.password = bcrypt.hashSync(
     req.body.password,
     bcrypt.genSaltSync(10)
   );
-  res.send(req.body);
+  // actually adding the data to MongoDB Atlas
+  User.create(req.body, (err, createdUser) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/");
+    }
+  });
 });
-
-module.exports = userRouter;
