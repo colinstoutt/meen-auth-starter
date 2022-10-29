526d1439659845f45e784b6e70ecb308ed8db270
cps97 <colinstoutt@gmail.com>
2022-10-29 10:58:26 -0700

    update server, add bcrypt

--- controllers/users.js
+++ controllers/users.js
@@ -1,1 +1,16 @@
+// Dependencies
+const express = require("express");
+const userRouter = express.Router();
+const User = require("../models/user.js");
+const bcrypt = require("bcrypt");
 
+userRouter.post("/", (req, res) => {
+  //overwrite the user password with the hashed password, then pass that in to our database
+  req.body.password = bcrypt.hashSync(
+    req.body.password,
+    bcrypt.genSaltSync(10)
+  );
+  res.send(req.body);
+});
+
+module.exports = userRouter;
--- server.js
+++ server.js
@@ -2,17 +2,34 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const userController = require("./controllers/users");
+
+// bcrypt
+const bcrypt = require("bcrypt");
+const hashedString = bcrypt.hashSync(
+  "yourPasswordStringHere",
+  bcrypt.genSaltSync(10)
+);
+
+// .env
 require("dotenv").config();
 const PORT = process.env.PORT;
 
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use("/users", userController);
+
+// mongoose connect
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+// mongoose connection status messages
 const db = mongoose.connection;
 db.on("error", (err) => console.log(err.message + " is mongod not running?"));
 db.on("connected", () => console.log("mongo connected"));
 db.on("disconnected", () => console.log("mongo disconnected"));
 
+// listener
 app.listen(PORT, () => console.log(`server is listening on port: ${PORT}`));
